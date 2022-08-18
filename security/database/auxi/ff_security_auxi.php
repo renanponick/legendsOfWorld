19b65f630f4d63f05aa789a28b04c07512a28d89
Renan <ponickrenan@gmail.com>
2022-08-17 21:50:40 -0300

    chore: change db from mysql to postgres

--- security/database/auxi/ff_security_auxi.php
+++ security/database/auxi/ff_security_auxi.php
@@ -1,18 +1,8 @@
 <?php
-/*
-	Autor: Renan Ponick;
-	Data: 19/03/2015;
-	
-Inicialmente é passado os padrões da Conexão com o BD
-ip é pra indica o local do BD - Servidor
-root = Usuário padrão, principal tem permissao total
-*/
-$servidor="127.0.0.1";
-$usuario="root";
-$senha="";
-$banco="ff_renan";
-
-//Salt - Incremento de segurança
-$salt = "#GvIlU&4VerDr3PJnT5";
-
+	$servidor="ec2-3-219-52-220.compute-1.amazonaws.com";
+	$banco="d1vq0ipvqmd4kq";
+	$usuario="rdwsjopnnbyrqv";
+	$porta="5432";
+	$senha="34fa8cb0744be4ca893e3d380534346d6149e2010bf878214fb63b09a61206bc";
+	$salt = "#GvIlU&4VerDr3PJnT5";
 ?>

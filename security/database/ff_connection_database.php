a9f444e7f2aa2ca1464d42cf0d8fbea83c9ff200
Renan <ponickrenan@gmail.com>
2022-08-18 23:08:38 -0300

    fix database

--- security/database/ff_connection_database.php
+++ security/database/ff_connection_database.php
@@ -1,19 +1,17 @@
 <?php
 	include "auxi/ff_security_auxi.php";
 
-	//$conexao = new PDO();
-	//$dsn = "pgsql:host=$servidor;port=5432;dbname=$banco;user=$usuario;password=$senha";
-	
-	$dsn = "pgsql:port=" . $porta .";dbname=" . $banco . ";user=" . $usuario . ";password=" . $senha . ";";
-	echo $dsn;
-	//create a pdo instance
+	$dsn = "pgsql:port=$porta;dbname=$banco;user=$usuario;password=$senha;host=$servidor";
 	$conexao = new PDO($dsn);
-	$conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-	$conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-	$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// $sql = 'SELECT * FROM users';
+	// $stmt = $conexao->prepare($sql);
+	// $stmt->execute();
+	// $rowCount = $stmt->rowCount();
+	// $details = $stmt->fetch();
+	// print_r ($details);
 
 	if(!$conexao){
 		die("Não foi possivel se conectar com o Banco de Dados - "+ $conexao);
 	}
-	$conexao -> set_charset('utf8');
 ?>

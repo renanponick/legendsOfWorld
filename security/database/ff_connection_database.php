f8ff3ed1a037b7b072158b4607aae8021804d000
Renan <ponickrenan@gmail.com>
2022-08-18 22:46:58 -0300

    try 1

--- security/database/ff_connection_database.php
+++ security/database/ff_connection_database.php
@@ -4,10 +4,10 @@
 	//$conexao = new PDO();
 	//$dsn = "pgsql:host=$servidor;port=5432;dbname=$banco;user=$usuario;password=$senha";
 	
-	$dsn = "pgsql:host=" . $servidor . ";port=" . $porta .";dbname=" . $banco . ";user=" . $usuario . ";password=" . $senha . ";";
+	$dsn = "pgsql:port=" . $porta .";dbname=" . $banco . ";user=" . $usuario . ";password=" . $senha . ";";
 	echo $dsn;
 	//create a pdo instance
-	$conexao = new PDO($dsn, $usuario, $senha);
+	$conexao = new PDO($dsn);
 	$conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 	$conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 	$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

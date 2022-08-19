e52c21a632d90898661749bbc9514b4cf24745f8
Renan <ponickrenan@gmail.com>
2022-08-18 22:37:11 -0300

    replace connection

--- security/database/ff_connection_database.php
+++ security/database/ff_connection_database.php
@@ -2,11 +2,14 @@
 	include "auxi/ff_security_auxi.php";
 
 	//$conexao = new PDO();
-	$dsn = "pgsql:host=$servidor;port=5432;dbname=$banco;user=$usuario;password=$senha";
-	echo $dsn;
-	$conexao = new PDO(
-		$dsn
-	);
+	//$dsn = "pgsql:host=$servidor;port=5432;dbname=$banco;user=$usuario;password=$senha";
+	
+	$dsn = "pgsql:host=" . $servidor . ";port=" . $porta .";dbname=" . $banco . ";user=" . $usuario . ";password=" . $senha . ";";
+	//create a pdo instance
+	$conexao = new PDO($dsn, $usuario, $senha);
+	$conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+	$conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+	$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	if(!$conexao){
 		die("Não foi possivel se conectar com o Banco de Dados - "+ $conexao);
